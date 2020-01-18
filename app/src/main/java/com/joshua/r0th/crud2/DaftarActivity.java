6c041113d496d30bc26d962ae75e6f441fbec51b
Joshua C.k <joshuaconstantine.k@gmail.com>
2020-01-19 02:08:52 +0700

    INITIAL

---
+++ app/src/main/java/com/joshua/r0th/crud2/DaftarActivity.java
@@ -0,0 +1,18 @@
+package com.joshua.r0th.crud2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DaftarActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daftar_activity);
+    }
+
+}
---
+++ app/src/main/java/com/joshua/r0th/crud2/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.joshua.r0th.crud2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+    public void goMain(View view){
+        Intent goMain = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(goMain);
+
+    }
+    public void goDaftar(View view){
+        setContentView(R.layout.daftar_activity);
+
+    }
+    public void goLogin(View view){
+        setContentView(R.layout.login_activity);
+
+    }
+}
