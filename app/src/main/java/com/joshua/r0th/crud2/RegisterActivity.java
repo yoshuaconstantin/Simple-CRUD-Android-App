890f26443ba756cd40d4c3fd17d9ae359859de5d
Joshua C.k <joshuaconstantine.k@gmail.com>
2020-01-19 18:03:46 +0700

    EXPERIMENTAL
    DONT DO

--- app/src/main/java/com/joshua/r0th/crud2/RegisterActivity.java
+++ app/src/main/java/com/joshua/r0th/crud2/RegisterActivity.java
@@ -1,90 +1,82 @@
 package com.joshua.r0th.crud2;
 
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.Spanned;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     //Declaration EditTexts
+    EditText editTextUserName;
     EditText editTextEmail;
     EditText editTextPassword;
 
     //Declaration TextInputLayout
+    TextInputLayout textInputLayoutUserName;
     TextInputLayout textInputLayoutEmail;
     TextInputLayout textInputLayoutPassword;
 
     //Declaration Button
-    Button buttonLogin;
+    Button buttonRegister;
 
     //Declaration SqliteHelper
     SqliteHelper sqliteHelper;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_register);
         sqliteHelper = new SqliteHelper(this);
-        initCreateAccountTextView();
+        initTextViewLogin();
         initViews();
-
-        //set click event of login button
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //Check user input is correct or not
                 if (validate()) {
-
-                    //Get values from EditText fields
+                    String UserName = editTextUserName.getText().toString();
                     String Email = editTextEmail.getText().toString();
                     String Password = editTextPassword.getText().toString();
 
-                    //Authenticate user
-                    User currentUser = sqliteHelper.Authenticate(new User(null, null, Email, Password));
+                    //Check in the database is there any user associated with  this email
+                    if (!sqliteHelper.isEmailExists(Email)) {
 
-                    //Check Authentication is successful or not
-                    if (currentUser != null) {
-                        Snackbar.make(buttonLogin, "Successfully Logged in!", Snackbar.LENGTH_LONG).show();
+                        //Email does not exist now add new user to database
+                        sqliteHelper.addUser(new User(null, UserName, Email, Password));
+                        Snackbar.make(buttonRegister, "User created successfully! Please Login ", Snackbar.LENGTH_LONG).show();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                finish();
+                            }
+                        }, Snackbar.LENGTH_LONG);
+                    }else {
 
-                        //User Logged in Successfully Launch You home screen activity
-                        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                        startActivity(intent);
-                        finish();
-
-                    } else {
-
-                        //User Logged in Failed
-                        Snackbar.make(buttonLogin, "Failed to log in , please try again", Snackbar.LENGTH_LONG).show();
-
+                        //Email exists with email input provided so show error user already exist
+                        Snackbar.make(buttonRegister, "User already exists with same email ", Snackbar.LENGTH_LONG).show();
                     }
+
+
                 }
             }
         });
-
-
     }
 
-    //this method used to set Create account TextView text and click event( maltipal colors
-    // for TextView yet not supported in Xml so i have done it programmatically)
-    private void initCreateAccountTextView() {
-        TextView textViewCreateAccount = (TextView) findViewById(R.id.daftarakun);
-        textViewCreateAccount.setOnClickListener(new View.OnClickListener() {
+    //this method used to set Login TextView click event
+    private void initTextViewLogin() {
+        TextView textViewLogin = (TextView) findViewById(R.id.textViewLogin);
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
@@ -93,22 +85,12 @@ public class LoginActivity extends AppCompatActivity {
     private void initViews() {
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUserName = (EditText) findViewById(R.id.editTextUserName);
         textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
         textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
-        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        textInputLayoutUserName = (TextInputLayout) findViewById(R.id.textInputLayoutUserName);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
-    }
-
-    //This method is for handling fromHtml method deprecation
-    @SuppressWarnings("deprecation")
-    public static Spanned fromHtml(String html) {
-        Spanned result;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
-        } else {
-            result = Html.fromHtml(html);
-        }
-        return result;
     }
 
     //This method is used to validate input given by user
@@ -116,8 +98,23 @@ public class LoginActivity extends AppCompatActivity {
         boolean valid = false;
 
         //Get values from EditText fields
+        String UserName = editTextUserName.getText().toString();
         String Email = editTextEmail.getText().toString();
         String Password = editTextPassword.getText().toString();
+
+        //Handling validation for UserName field
+        if (UserName.isEmpty()) {
+            valid = false;
+            textInputLayoutUserName.setError("Please enter valid username!");
+        } else {
+            if (UserName.length() > 5) {
+                valid = true;
+                textInputLayoutUserName.setError(null);
+            } else {
+                valid = false;
+                textInputLayoutUserName.setError("Username is to short!");
+            }
+        }
 
         //Handling validation for Email field
         if (!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
@@ -142,8 +139,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
 
+
         return valid;
     }
-
-
 }
