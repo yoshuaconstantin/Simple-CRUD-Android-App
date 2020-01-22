3a4114e7e146dc56783d61ef761513041705c818
Joshua C.k <joshuaconstantine.k@gmail.com>
2020-01-22 23:42:36 +0700

    TRACKING
    - try do add database
    - WIll continue later

---
+++ app/src/main/java/com/joshua/r0th/crud2/database1.java
@@ -0,0 +1,127 @@
+package com.joshua.r0th.crud2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class database1 extends SQLiteOpenHelper {
+
+    //nama database
+
+    public static final String DATABASE_NAME = "JENTIK.db";
+
+    //nama table
+
+    public static final String TABLE_NAME = "data_jentik";
+
+    //versi database
+
+    private static final int DATABASE_VERSION = 1;
+
+    //table field
+
+    public static final String COL_1 = "NomorRumah";
+
+    public static final String COL_2 = "JentikDalam";
+
+    public static final String COL_3 = "JentikLuar";
+
+
+
+
+    public database1(Context context) {
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+    }
+
+
+
+    @Override
+
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table data_jentik (NomorRumah integer null ," +
+
+                "JentikDalam integer null," +
+
+                "JentikLuar integer null);");
+
+    }
+
+
+
+    @Override
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+
+        onCreate(db);
+
+    }
+
+
+
+    //metode untuk tambah data
+
+    public boolean insertData(String NomorRumah, String JentikDalam, String JentikLuar) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_1,NomorRumah);
+
+        contentValues.put(COL_2,JentikDalam);
+
+        contentValues.put(COL_3,JentikLuar);
+
+
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1)
+
+            return false;
+
+        else
+
+            return true;
+
+    }
+
+
+
+    //metode untuk mengambil data
+
+    public Cursor getAllData() {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor res = db.rawQuery("select * from data_jentik", null);
+
+        return res;
+
+    }
+
+
+
+
+
+
+    //metode untuk menghapus data
+
+    public int deleteData (String id) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        return db.delete(TABLE_NAME, "NoRumah = ?", new String[] {id});
+
+    }
+
+}
--- app/src/main/java/com/joshua/r0th/crud2/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/joshua/r0th/crud2/ui/gallery/GalleryFragment.java
@@ -1,35 +1,181 @@
 package com.joshua.r0th.crud2.ui.gallery;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.joshua.r0th.crud2.R;
+import com.joshua.r0th.crud2.database1;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends AppCompatActivity {
 
-    private GalleryViewModel galleryViewModel;
+    database1 myDb;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_pantauan, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    EditText editNomorRumah,editJentikDalam,editJentikLuar,editTextId;
+
+    Button btnAddData;
+
+    Button btnViewAll;
+
+
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        myDb = new database1(this);
+
+        editNomorRumah = (EditText)findViewById(R.id.nomorrmh);
+
+        editJentikDalam = (EditText)findViewById(R.id.jentikdirumah);
+
+        editJentikLuar = (EditText)findViewById(R.id.jentikdiluarrumah);
+
+
+        btnAddData = (Button)findViewById(R.id.tambahdata);
+
+        btnViewAll = (Button)findViewById(R.id.lihatdata);
+
+
+
+        AddData();
+
+        viewAll();
+
+
+
     }
+
+
+
+    //fungsi tambah
+
+    public void AddData() {
+
+        btnAddData.setOnClickListener(
+
+                new View.OnClickListener() {
+
+                    @Override
+
+                    public void onClick(View v) {
+
+                        boolean isInserted = myDb.insertData(editNomorRumah.getText().toString(),
+
+                                editJentikDalam.getText().toString(),
+
+                                editJentikLuar.getText().toString() );
+
+                        if(isInserted == true)
+
+                            Toast.makeText(GalleryFragment.this,"Data Iserted",Toast.LENGTH_LONG).show();
+
+                        else
+
+                            Toast.makeText(GalleryFragment.this,"Data Not Iserted",Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+        );
+
+    }
+
+
+
+    //fungsi menampilkan data
+
+    public void viewAll() {
+
+        btnViewAll.setOnClickListener(
+
+                new View.OnClickListener(){
+
+                    @Override
+
+                    public void onClick(View v) {
+
+                        Cursor res = myDb.getAllData();
+
+                        if(res.getCount() == 0) {
+
+                            // show message
+
+                            showMessage("Error","Noting Found");
+
+                            return;
+
+                        }
+
+
+
+                        StringBuffer buffer = new StringBuffer();
+
+                        while (res.moveToNext() ) {
+
+                            buffer.append("NomorRumah :"+ res.getString(0)+"\n");
+
+                            buffer.append("JentikDalam :"+ res.getString(1)+"\n");
+
+                            buffer.append("JentikLuar :"+ res.getString(2)+"\n");
+
+
+
+                        }
+
+
+
+                        // show all data
+
+                        showMessage("Data",buffer.toString());
+
+                    }
+
+                }
+
+        );
+
+    }
+
+
+
+    //membuat alert dialog
+
+    public void showMessage(String title, String Message){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setCancelable(true);
+
+        builder.setTitle(title);
+
+        builder.setMessage(Message);
+
+        builder.show();
+
+    }
+
 }
+
+
